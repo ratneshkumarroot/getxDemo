7305216c9b6628c9e495429acd14014e4cfc45c2
ratnesh <ratneshkumar.singh@rootinfosolutions.net>
2023-11-02 15:33:53 +0530

    test code magic

--- lib/main.dart
+++ lib/main.dart
@@ -42,14 +42,3 @@ class Home extends StatelessWidget {
        );
   }
 }
-
-// class Other extends StatelessWidget {
-//   // You can ask Get to find a Controller that is being used by another page and redirect you to it.
-//   final Controller c = Get.find();
-//
-//   @override
-//   Widget build(context){
-//     // Access the updated count variable
-//     return Scaffold(body: Center(child: Text("${c.count}")));
-//   }
-// }
